f16265534ae9ffa5530a535e278611dcbfdda3fe
Seppi <schuhmannsebastian@gmail.com>
2020-06-01 18:45:13 +0200

    Added Enemy and Agent logic

---
+++ A.I. Shooting Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int startingHealth = 100;
+
+    private int CurrentHealth;
+    private Vector3 StartPosition; 
+    
+    private void Start()
+    {
+        StartPosition = transform.position;
+        CurrentHealth = startingHealth;
+    }
+    
+    public void GetShot(int damage, ShootingAgent shooter)
+    {
+        ApplyDamage(damage, shooter);
+    }
+    
+    private void ApplyDamage(int damage, ShootingAgent shooter)
+    {
+        CurrentHealth -= damage;
+
+        if (CurrentHealth <= 0)
+        {
+            Die(shooter);
+        }
+    }
+    
+    private void Die(ShootingAgent shooter)
+    {
+        Debug.Log("I died!");
+        shooter.RegisterKill();
+        Respawn();
+    }
+    
+    private void Respawn()
+    {
+        CurrentHealth = startingHealth;
+        transform.position = StartPosition;
+    }
+}
---
+++ A.I. Shooting Game/Assets/Scripts/ShootingAgent.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class ShootingAgent : Agent
+{
+    public int score = 0;
+    
+    public Transform shootingPoint;
+    public int minStepsBetweenShots = 50;
+    public int damage = 100;
+    
+    private bool ShotAvaliable = true;
+    private int StepsUntilShotIsAvaliable = 0;
+    
+    private Vector3 StartingPosition;
+    private Rigidbody Rb;
+    
+    
+    private void Shoot()
+    {
+        if (!ShotAvaliable)
+            return;
+        
+        var layerMask = 1 << LayerMask.NameToLayer("Enemy");
+        var direction = transform.forward;
+        
+        Debug.Log("Shot");
+        Debug.DrawRay(shootingPoint.position, direction * 200f, Color.green, 2f);
+        
+        if (Physics.Raycast(shootingPoint.position, direction, out var hit, 200f, layerMask))
+        {
+            hit.transform.GetComponent<Enemy>().GetShot(damage, this);
+        }
+
+        ShotAvaliable = false;
+        StepsUntilShotIsAvaliable = minStepsBetweenShots;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!ShotAvaliable)
+        {
+            StepsUntilShotIsAvaliable--;
+
+            if (StepsUntilShotIsAvaliable <= 0)
+                ShotAvaliable = true;
+        }
+    }
+
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (Mathf.RoundToInt(vectorAction[0]) >= 1)
+        {
+            Shoot();
+        }
+    }
+    
+    public override void Initialize()
+    {
+        StartingPosition = transform.position;
+        Rb = GetComponent<Rigidbody>();
+    }
+    
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetKey(KeyCode.P) ? 1f : 0f;
+        //transform.rotation.SetLookRotation();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        Debug.Log("Episode Begin");
+        transform.position = StartingPosition;
+        Rb.velocity = Vector3.zero;
+        ShotAvaliable = true;
+    }
+
+    public void RegisterKill()
+    {
+        score++;
+        AddReward(1.0f);
+        EndEpisode();
+    }
+}
